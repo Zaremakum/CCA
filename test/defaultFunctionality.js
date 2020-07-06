f436d668ece765e4033a44e821e49d2c559ef079
13017 <zaremakum@gmail.com>
2020-07-06 15:49:51 -0400

    up to TC 047

--- test/defaultFunctionality.js
+++ test/defaultFunctionality.js
@@ -15,36 +15,66 @@ describe('Default counter functionality', function () {
         const countValue = $(selectorCnt.countValue).getText();
         expect(countValue).toEqual(expectedDCF.countValueTC041);
     })
+
+
     it('TC-042 LLF accept 1', function () {
-        const button = $(selectorCnt.lowerLimitField);
-        button.click();
-        const input = $(selectorCnt.lowerInputField);
-        input.click();
-        browser.keys('Backspace');
-        browser.keys(expectedDCF.inputMin.toString());
+        inputNumber('left', expectedDCF.inputMin);
         const result = $(selectorCnt.errorMsg).isDisplayed();
         expect(result).toEqual(false);
     })
 
     it('TC-043 ULF accept 9', function () {
-        //const inputLLF = $(selectorCnt.lowerInputField);
-       // inputLLF.getText();
-        const button = $(selectorCnt.upperLimitField);
-        button.click();
-        const input = $(selectorCnt.upperInputField);
-        input.click();
-        browser.keys('Backspace');
-        browser.keys(expectedDCF.inputMax.toString());
+        inputNumber('right', expectedDCF.inputMax);
         const result = $(selectorCnt.errorMsg).isDisplayed();
         expect(result).toEqual(false);
     })
-    /*
+    // it('TC-042 LLF accept 1', function () {
+    //     const button = $(selectorCnt.lowerLimitField);
+    //     button.click();
+    //     const input = $(selectorCnt.lowerInputField);
+    //     input.click();
+    //     browser.keys('Backspace');
+    //     browser.keys(expectedDCF.inputMin.toString());
+    //     const result = $(selectorCnt.errorMsg).isDisplayed();
+    //     expect(result).toEqual(false);
+    // })
+    //
+    // it('TC-043 ULF accept 9', function () {
+    //     //const inputLLF = $(selectorCnt.lowerInputField);
+    //    // inputLLF.getText();
+    //     const button = $(selectorCnt.upperLimitField);
+    //     button.click();
+    //     const input = $(selectorCnt.upperInputField);
+    //     input.click();
+    //     browser.keys('Backspace');
+    //     browser.keys(expectedDCF.inputMax.toString());
+    //     const result = $(selectorCnt.errorMsg).isDisplayed();
+    //     expect(result).toEqual(false);
+    // })
+
         it('TC-044 LLF = 1 and ULF = 1 gives 2 black buttons', function () {
-            browser.pause(1000);
-            //inputNumber('left', expectedDCF.inputMin);
+            browser.refresh();
+            inputNumber('left', expectedDCF.inputMin);
             inputNumber('right', expectedDCF.inputMin);
             const actual = $$(selectorCnt.blackBtn).filter(el => el.isDisplayed()).length;
-            expect(actual).toEqual(expectedDCF.countValueTC041);
+            expect(actual).toEqual(+expectedDCF.countValueTC041);
         })
-    */
+    //      it('TC 045 LLF = 9 and ULF = 9 gives 2 black buttons', function () { // NE PROHODIT ?!!!
+    //          browser.refresh();
+    //         inputNumber( 'left', expectedDCF.inputMax);
+    //         inputNumber('right', expectedDCF.inputMax);
+    //         const actual = $$(selectorCnt.blackBtn).filter(el => el.isDisplayed()).length;
+    //         expect(actual).toEqual(expectedDCF.countValueTC041);
+    // })
+        it('TC - 046 Reset button works ', function () {
+            $(selectorCnt.resetBtn).click();
+            const actual = $(selectorCnt.countValue).getText();
+            expect(actual).toEqual(expectedDCF.countValue);
+    })
+        it('TC - 047 Delete button works', function () {
+            $(selectorCnt.deleteBtn).click();
+            const actual = $(selectorCnt.countValue).isDisplayed();
+            expect(actual).toEqual(false);
+    })
+
 });
